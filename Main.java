7d651073beccf25c5a0d423478f4c23f8ac5b509
Devpabloedu <158394108+devPabloEdu@users.noreply.github.com>
2024-06-12 18:44:47 -0300

    criando repositorio

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static  void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello  world!");
 
         byte variavelByte = 127; //o byte só vai até o 127
         //short : -32768 até o 32767
