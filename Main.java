ba3a84b5a9ed20a3a0bf982f23241088f17bbfa5
Devpabloedu <158394108+devPabloEdu@users.noreply.github.com>
2024-06-12 18:42:52 -0300

    Curso poo

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static  void main(String[] args) {
+        System.out.println("Hello world!");
+
+        byte variavelByte = 127; //o byte só vai até o 127
+        //short : -32768 até o 32767
+        //char :  0 a 65535
+        //int : -2b a 2bilhoes
+        //long de -9trilhoes a 9trilhoes
+
+        int variavelInt = 2147483;
+
+        System.out.println(variavelInt);
+        System.out.println(variavelByte);
+    }
+}
