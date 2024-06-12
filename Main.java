3bb50414b710f080c576c9e5f52b63cd4dbc101d
Devpabloedu <158394108+devPabloEdu@users.noreply.github.com>
2024-06-12 19:09:39 -0300

    aprendendo a criar um objeto e a diferença entre objetos e classes

--- Main.java
+++ Main.java
@@ -1,17 +1,28 @@
+import Animais.Cachorro;
+
 public class Main {
 
     public static  void main(String[] args) {
-        System.out.println("Hello  world!");
+        //System.out.println("Hello  world!");
 
-        byte variavelByte = 127; //o byte só vai até o 127
+        //byte variavelByte = 127; //o byte só vai até o 127
         //short : -32768 até o 32767
         //char :  0 a 65535
         //int : -2b a 2bilhoes
         //long de -9trilhoes a 9trilhoes
 
-        int variavelInt = 2147483;
+        //int variavelInt = 2147483;
 
-        System.out.println(variavelInt);
-        System.out.println(variavelByte);
+       //System.out.println(variavelInt);
+        //System.out.println(variavelByte);
+        Cachorro cachorro1 = new Cachorro();
+
+        cachorro1.nome = "Puppy";
+        cachorro1.cor = "marrom";
+        cachorro1.altura = 25;
+        cachorro1.peso = 5.5;
+        cachorro1.tamanhoDoRabo = 5;
+
+        System.out.println(cachorro1.altura);
     }
 }
