6b53a17f5011328d764db0fa42dfca51c2a1902c
Devpabloedu <158394108+devPabloEdu@users.noreply.github.com>
2024-06-12 21:03:46 -0300

    construtores, getters and setters

--- Animais/Cachorro.java
+++ Animais/Cachorro.java
@@ -1,14 +1,78 @@
 package Animais;
 
 public class Cachorro {
+//atributos
+    private String nome;
+    private String cor;
+    private int altura;
+    private double peso;
+    private int tamanhoDoRabo;
+    private String estadoDeEspirito;
 
-    public String nome;
-    public String cor;
-    public int altura;
-    public double peso;
-    public int tamanhoDoRabo;
+    //construtores
 
-    public String estadoDeEspirito;
+    public Cachorro(){
+
+    }
+
+    public Cachorro(String nome, String cor, double peso, int altura, int tamanhoDoRabo, String estadoDeEspirito) {
+        this.nome = nome;
+        this.cor = cor;
+        this.peso = peso;
+        this.altura = altura;
+        this.tamanhoDoRabo = tamanhoDoRabo;
+        this.estadoDeEspirito = estadoDeEspirito;
+    }
+
+    //metodos
+    public String getNome(){
+        return this.nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+    public String getEstadoDeEspirito() {
+        return estadoDeEspirito;
+    }
+
+    public void setEstadoDeEspirito(String estadoDeEspirito) {
+        this.estadoDeEspirito = estadoDeEspirito;
+    }
+
+    public int getTamanhoDoRabo() {
+        return tamanhoDoRabo;
+    }
+
+    public void setTamanhoDoRabo(int tamanhoDoRabo) {
+        this.tamanhoDoRabo = tamanhoDoRabo;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
 
     public void comer(){}
     public void Latir(){
@@ -19,13 +83,29 @@ public class Cachorro {
     }
 
     public String interagir(String acao) {
-        if(acao.equals("carinho")){
-            this.estadoDeEspirito ="feliz, pois voce fez carinho nele";
-        } else if(acao.equals("vai dormir")) {
-            this.estadoDeEspirito = "bravo, pois voce o mandou dormir";
-        } else {
-            this.estadoDeEspirito = "neutro, pois voce não interagiu com ele";
+
+        switch (acao){
+            case "carinho" : this.estadoDeEspirito = "feliz, pois recebeu carinho";
+            break;
+            case "vai dormir" : this.estadoDeEspirito = "bravo, pois voce o mandou dormir";
+            break;
+            case "pisou na patinha" : this.estadoDeEspirito = "triste, pois voce pisou na patinha dele";
+            break;
+            default: this.estadoDeEspirito = "neutro, pois voce nao interagiu com ele";
+
         }
         return  estadoDeEspirito;
+
+
+
+
+//        if(acao.equals("carinho")){
+//            this.estadoDeEspirito ="feliz, pois voce fez carinho nele";
+//        } else if(acao.equals("vai dormir")) {
+//            this.estadoDeEspirito = "bravo, pois voce o mandou dormir";
+//        } else {
+//            this.estadoDeEspirito = "neutro, pois voce não interagiu com ele";
+//        }
+//        return  estadoDeEspirito;
     }
 }
--- Main.java
+++ Main.java
@@ -3,29 +3,19 @@ import Animais.Cachorro;
 public class Main {
 
     public static  void main(String[] args) {
-        //System.out.println("Hello  world!");
-
-        //byte variavelByte = 127; //o byte só vai até o 127
-        //short : -32768 até o 32767
-        //char :  0 a 65535
-        //int : -2b a 2bilhoes
-        //long de -9trilhoes a 9trilhoes
-
-        //int variavelInt = 2147483;
-
-       //System.out.println(variavelInt);
-        //System.out.println(variavelByte);
         Cachorro cachorro1 = new Cachorro();
+        Cachorro cachorro2 = new Cachorro("rex", "preto", 1.5, 5, 8, "feliz" );
+        System.out.println(cachorro2.getNome());
 
-        cachorro1.nome = "Puppy";
-        cachorro1.cor = "marrom";
-        cachorro1.altura = 25;
-        cachorro1.peso = 5.5;
-        cachorro1.tamanhoDoRabo = 5;
+        cachorro1.setNome("Puppy");
+        cachorro1.setCor("marrom");
+        cachorro1.setAltura(25);
+        cachorro1.setPeso(5.5);
+        cachorro1.setTamanhoDoRabo(5);
 
         cachorro1.Latir();
         System.out.println("O cachorro pegou uma " + cachorro1.pegar());//se nao tivesse dentro do system out iria retornar mas n escreveria na tela
 
-        System.out.println("o cachorro está " + cachorro1.interagir("carinho"));
+        System.out.println("o cachorro está " + cachorro1.interagir("pisou na patinha"));
     }
 }
